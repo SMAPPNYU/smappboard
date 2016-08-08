227198074934a7e80be6f75064e97403dcd3434d
yvan <yvanscher@gmail.com>
2016-08-08 17:56:38 -0400

    added filters to data set page

--- smappboard/app.py
+++ smappboard/app.py
@@ -39,14 +39,20 @@ def single_dataset(data_set_name):
     mongo = pymongo.MongoClient(app.config['SMAPPBOARD_MONGO_HOST'], int(app.config['SMAPPBOARD_MONGO_PORT']))
     db_size = mongo[data_set_name].command("dbstats")
     db_size_gb = humanize.naturalsize(int(db_size['dataSize']))
+
     metadata = mongo[data_set_name]['metadata'].find_one({})
     data_type = metadata['data_type'] if metadata else 'tweets'
     dataset_platform = metadata['platform'] if metadata else 'twitter'
+    filter_list = [{"filter_type": filt['filter_type'], "filter":filt['value']} for filt in mongo[data_set_name]['tweets_filter_criteria'].find({})]
+    # if new format  does not work
+    # check for old format
+    if len(filter_list) == 0:
+        filter_list = [{"filter_type": filt['filter_type'], "filter":filt['value']} for filt in mongo[data_set_name]['tweets_filter_criteria'].find({})]
+
     pysmap_dataset = pysmap.SmappDataset(['mongo', app.config['SMAPPBOARD_MONGO_HOST'], app.config['SMAPPBOARD_MONGO_PORT'], app.config['MONGO_READONLY_USER'], app.config['MONGO_READONLY_PASS'], data_set_name], collection_regex='(^data$|^tweets$|^tweets_\d+$)')
     num_tweets = sum([smappdragon_col.mongo_collection.count() for smappdragon_col in pysmap_dataset.collections])   
-    return render_template('dataset.html', dataset_name=data_set_name, dataset_size=db_size_gb, dataset_platform=dataset_platform, data_type=data_type, num_tweets=num_tweets)
+    return render_template('dataset.html', dataset_name=data_set_name, dataset_size=db_size_gb, dataset_platform=dataset_platform, data_type=data_type, num_tweets=num_tweets, filter_list=filter_list)
 
-# a password protected page 
 @app.route('/access/<string:data_set_name>')
 def dataset_dashboard(data_set_name):
     return data_set_name
